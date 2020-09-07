c6b220ec33aee9ca8894c8a1ad7eef3e0db03647
YobaMalik <60839836+YobaMalik@users.noreply.github.com>
2020-09-07 23:02:58 +0300

    Update UploadApplication.java

--- src/main/java/com/example/demo/UploadApplication.java
+++ src/main/java/com/example/demo/UploadApplication.java
@@ -10,26 +10,6 @@ public class UploadApplication  {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(UploadApplication.class, args);
-		//	Process prc=Runtime.getRuntime().exec(new String[]{"shutdown","now"});
-		//	System.exit(0);
-	/*	PipeCalculation pipe=new PipeCalculation(10,120,50);
-		System.out.println(pipe.getPipeThickness());
-		System.out.println(pipe.getPipePressure());
-
-	 */
-	/*
-
-	(double desPressure, double strValue,double outDiam,double thickness, double radius,String elbowType)
-	ElbowForm pipe=new ElbowForm();
-
-	AbstractTensionCalc test1=new ElementsVisitor().visit(pipe);
-	System.out.print(test1.getClass());
-
-	 */
-	//тест
 	}
 }
 
-//chdir C:\Users\Yoba\IdeaProjects\Upload v18 + react\src\main\resources\static
-//npm run dev
-//npm run build
